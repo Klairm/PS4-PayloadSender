ce805104b3f51e8a548665a428b5eaff399f74cb
Ivan <ivangardis@hotmail.com>
2021-12-24 12:37:39 +0100

    refactor: log some error info

--- App.js
+++ App.js
@@ -47,7 +47,7 @@ export default function App() {
 									setPayload(file);
 								}
 							} catch (error) {
-								alert(error);
+								alert(`There was an error! DEBUG: ${error}`);
 							}
 						}}
 					/>
